7a6e160e402ccda0e19a20952a9a09cfb8b8156a
SudarshanPaliwal <59984906+SudarshanPaliwal@users.noreply.github.com>
2021-04-23 10:27:21 -0400

    Update q1_kmeans.m

--- q1_kmeans.m
+++ q1_kmeans.m
@@ -14,16 +14,9 @@ r1 = mvnrnd(mu1,sigma1,500);
 r2 = mvnrnd(mu2,sigma2,500);
 r3 = mvnrnd(mu3,sigma3,500);
 r4 = mvnrnd(mu4,sigma4,500);
-X = [r1; r2;r3;r4]
+X = [r1; r2;r3;r4];
 
 
-
-
-% X = [randn(100,2)*0.75+ones(100,2)*5;
-%     randn(100,2)*0.75-ones(100,2)*5];
-%     randn(100,2)*0.75-ones(100,2)*2
-%     randn(100,2)*0.75];
-%     
 %plot data 
 figure;
 plot(X(:,1),X(:,2),'.');
@@ -31,7 +24,19 @@ title 'Randomly Generated Data';
 %call own implementation of k means 
 k = 4;
 [cluster_idx,centers] = mykmeans_implem(X,k);
-% [cluster_idx,centers] = kmeans(X,k);
+%Calculate SSE 
+SSE = 0;
+for i = 1:k    
+    idx = find(cluster_idx == i);
+    error= norm(X((idx),:) - centers(i,:),'fro')^2;    
+    SSE = SSE + error;
+end
+sprintf('SSE for k-means is: %d',SSE)
+
+
+
+
+
 
 
 %plot clusters with centroids
@@ -42,8 +47,8 @@ plot(X(cluster_idx==2,1),X(cluster_idx==2,2),'g.','MarkerSize',12)
 plot(X(cluster_idx==3,1),X(cluster_idx==3,2),'b.','MarkerSize',12)
 plot(X(cluster_idx==4,1),X(cluster_idx==4,2),'y.','MarkerSize',12)
 plot(centers(:,1),centers(:,2),'kx','MarkerSize',15,'LineWidth',3) 
-legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4','Centroids','Location','Best')
-%legend('Cluster 1','Cluster 2','Cluster 3','Centroids','Location','Best')
+%legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4','Centroids','Location','Best')
+legend('Cluster 1','Cluster 2','Cluster 3','Centroids','Location','Best')
 title 'Cluster Assignments and Centroids k-means'
 hold off
 
@@ -54,14 +59,23 @@ Y = X*X'; %compute Y
 [V,D] = eig(Y);%perform eigen value decomposition
 [d,ind] = sort((diag(D)),'descend');
 spectral_X = V(:,ind(1:k,:));
-%spectral_X = Vs';
+
+
 [cluster_idx,centers] = mykmeans_implem(spectral_X,k);
-%[cluster_idx,centers] = kmeans(spectral_X,k);
 centers = zeros(k,2);
 for j = 1:k
     idx = find(cluster_idx == j);
     centers(j,:) = mean(X((idx),:));    
 end
+
+%Calculate SSE 
+SSE = 0;
+for i = 1:k    
+    idx = find(cluster_idx == i);
+    error= norm(X((idx),:) - centers(i,:),'fro')^2;    
+    SSE = SSE + error;
+end
+sprintf('SSE for my spectral k-means is: %d',SSE)
 
 %plot clusters with centroids
 figure;
@@ -71,8 +85,8 @@ plot(X(cluster_idx==2,1),X(cluster_idx==2,2),'g.','MarkerSize',12)
 plot(X(cluster_idx==3,1),X(cluster_idx==3,2),'b.','MarkerSize',12)
 plot(X(cluster_idx==4,1),X(cluster_idx==4,2),'y.','MarkerSize',12)
 plot(centers(:,1),centers(:,2),'kx','MarkerSize',15,'LineWidth',3)
-legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4','Centroids','Location','Best')
-%legend('Cluster 1','Cluster 2','Cluster 3','Centroids','Location','Best')
+%legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4','Centroids','Location','Best')
+legend('Cluster 1','Cluster 2','Cluster 3','Centroids','Location','Best')
 title 'Cluster Assignments and Centroids spectral k means'
 hold off
 
